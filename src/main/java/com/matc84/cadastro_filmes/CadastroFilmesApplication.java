8d0750d6ea12e4d2372099c273d76967d51fd898
reynanwq <reynanwq@gmail.com>
2024-11-29 14:16:59 -0300

    Initialize Spring Boot project

---
+++ src/main/java/com/matc84/cadastro_filmes/CadastroFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.matc84.cadastro_filmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matc84/cadastro_filmes/CadastroFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matc84.cadastro_filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
